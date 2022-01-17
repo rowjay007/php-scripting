2a67534d6f86cd98262ba26ef33edfbba188954d
rowjay007 <rowlives@gmail.com>
2022-01-17 12:15:21 +0100

    first php hello from the local server

---
+++ site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+echo("Rowjay's Website");
+echo"<hr>";
+echo"<p>This is my site</p>";
+    ?>
+</body>
+
+</html>
